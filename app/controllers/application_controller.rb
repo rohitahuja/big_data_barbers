ad58382d2bfca45817bcd1184008a00f40781c91
Rohit Ahuja <rahuja95@gmail.com>
2015-06-14 00:50:09 -0400

    Added attribute params and nested params for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,12 @@ class ApplicationController < ActionController::Base
 protected
 
   def configure_permitted_parameters
-  	devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation) }
-  	devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation) }
+  	devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(
+  		:first_name, :last_name, :email, :password, :password_confirmation,
+  		professional_account_attributes: [:phone_number, :bio, 
+        workplace_attributes: [:name, :street, :city, :zip, :state, :country] ]) }
+  	devise_parameter_sanitizer.for(:account_update) { |u| u.permit(
+  		:first_name, :last_name, :email, :password, :password_confirmation) }
 	end
 
 end
