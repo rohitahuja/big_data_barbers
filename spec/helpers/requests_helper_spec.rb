655040987e63436ec1b9d90e8e7edc620e56e818
Rohit Ahuja <rahuja95@gmail.com>
2015-07-26 14:12:05 -0400

    Adjusted tests for new authentication library

--- spec/helpers/requests_helper_spec.rb
+++ spec/helpers/requests_helper_spec.rb
@@ -9,8 +9,15 @@ module RequestsHelper
 		def login_user
 			user = FactoryGirl.create(:user)
 			params = { email: user.email, password: user.password }
-			post '/api/v1/sign_in', params
+			post '/api/v1/auth/sign_in', params
 			user
+		end
+
+		def request_header
+			header = response.header
+			{ 'uid' => header['uid'], 
+				'access-token' => header['access-token'], 
+				'client' => header['client'] }
 		end
 	end
 end
--- spec/requests/session_spec.rb
+++ spec/requests/session_spec.rb
@@ -2,43 +2,39 @@ require 'rails_helper'
 include RequestsHelper::SessionsHelper
 
 describe 'Session' do
-	context 'POST /api/v1/sign_in' do
+	context 'POST /api/v1/auth/sign_in' do
 		it 'signs in successfully' do
 	    user = login_user
 
 	    expect(response).to be_success
-	    expect(json['email']).to eq(user.email) # check to make sure the right amount of messages are returned
+	    expect(json['data']['email']).to eq(user.email) # check to make sure the right amount of messages are returned
 	  end
 
 	  it 'does not sign in with wrong credentials' do
 	  	# will not create a user
 	  	params = { email: Faker::Internet.email, password: Faker::Internet.password }
 
-	  	post '/api/v1/sign_in', params
-
+	  	post '/api/v1/auth/sign_in', params
 	  	expect(response).to_not be_success
-	  	expect(json['message']).to_not be_empty
+	  	expect(json['errors']).to_not be_empty
 	  end
 	end
   
-  context 'DELETE /api/v1/sign_out' do
+  context 'DELETE /api/v1/auth/sign_out' do
   	it 'signs out successfully' do
 	  	user = login_user
 
-	  	params = { email: user.email, access_token: user.access_token }
-	  	post '/api/v1/sign_out', params
-
+	  	delete '/api/v1/auth/sign_out', nil, request_header
 	  	expect(response).to be_success
 	  end
 
 	  it 'does not sign out when not initially signed in' do
 	  	user = FactoryGirl.create(:user)
 
-	  	params = { email: user.email, access_token: user.access_token }
-	  	post '/api/v1/sign_out', params
+	  	delete '/api/v1/auth/sign_out'
 
 	  	expect(response).to_not be_success
-	  	expect(json['message']).to_not be_empty
+	  	expect(json['errors']).to_not be_empty
 	  end
   end
   
