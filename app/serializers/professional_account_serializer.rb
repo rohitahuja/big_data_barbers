34a04033be714e139099171391c2dab6ddfd31bd
Rohit Ahuja <rahuja95@gmail.com>
2015-07-31 00:42:18 -0400

    Fixed routes and folder structure

--- app/serializers/professional_account_serializer.rb
+++ app/serializers/professional_account_serializer.rb
@@ -1,12 +1,16 @@
 class ProfessionalAccountSerializer < ActiveModel::Serializer
-  attributes :id, :name, :phone_number, :bio, :workplace
+  attributes :id, :name, :phone_number, :bio, :workplace, :schedule_id
 
   def name
   	object.user.name
   end
 
   def workplace
-  	Workplace.where(id: object.workplace_id).pluck(:name, :address_1, :address_2, :city, :zip, :country).first
+  	Workplace.where(id: object.workplace_id).select([:name, :address_1, :address_2, :city, :zip, :country]).first
+  end
+
+  def schedule_id
+  	Schedule.find_by_professional_account_id(object.id).id
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,13 @@ Rails.application.routes.draw do
 
   mount_devise_token_auth_for 'User', at: 'api/v1/auth'
 
-  scope :api, defaults: { format: :json } do
+  namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :professional_accounts, only: [:show, :update]
+      resources :professional_accounts, only: [:show, :update] do
+      	resources :schedule, only: [:show, :update], shallow: true
+      	resources :appointments, only: [:index, :show, :create, :update], shallow: true
+      	resources :post, only: [:index, :show, :create, :destroy], shallow: true
+      end
     end
   end
   
