3b7b43a06f72d8116c29ba5cfece6f105a90ce09
Rohit Ahuja <rahuja95@gmail.com>
2015-09-12 05:05:29 -0400

    Updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150711225722) do
+ActiveRecord::Schema.define(version: 20150906012823) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,12 +58,29 @@ ActiveRecord::Schema.define(version: 20150711225722) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "image"
-    t.integer  "professional_id"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.integer  "professional_profile_id"
+    t.integer  "service_id"
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
   end
 
-  add_index "posts", ["professional_id"], name: "index_posts_on_professional_id", using: :btree
+  add_index "posts", ["professional_profile_id"], name: "index_posts_on_professional_profile_id", using: :btree
+  add_index "posts", ["service_id"], name: "index_posts_on_service_id", using: :btree
+
+  create_table "professional_profiles", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "phone_number"
+    t.text     "bio"
+    t.string   "image"
+    t.integer  "professional_id"
+    t.integer  "professional_profile_id"
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+  end
+
+  add_index "professional_profiles", ["professional_id"], name: "index_professional_profiles_on_professional_id", using: :btree
+  add_index "professional_profiles", ["professional_profile_id"], name: "index_professional_profiles_on_professional_profile_id", using: :btree
 
   create_table "professionals", force: :cascade do |t|
     t.string   "provider",               default: "email", null: false
@@ -82,21 +99,16 @@ ActiveRecord::Schema.define(version: 20150711225722) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
+    t.integer  "shop_id"
     t.text     "tokens"
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "phone_number"
-    t.text     "bio"
-    t.string   "profile_image"
-    t.integer  "workplace_id"
     t.datetime "created_at",                               null: false
     t.datetime "updated_at",                               null: false
   end
 
   add_index "professionals", ["email"], name: "index_professionals_on_email", unique: true, using: :btree
   add_index "professionals", ["reset_password_token"], name: "index_professionals_on_reset_password_token", unique: true, using: :btree
+  add_index "professionals", ["shop_id"], name: "index_professionals_on_shop_id", using: :btree
   add_index "professionals", ["uid", "provider"], name: "index_professionals_on_uid_and_provider", unique: true, using: :btree
-  add_index "professionals", ["workplace_id"], name: "index_professionals_on_workplace_id", using: :btree
 
   create_table "schedules", force: :cascade do |t|
     t.integer  "professional_id"
@@ -106,7 +118,27 @@ ActiveRecord::Schema.define(version: 20150711225722) do
 
   add_index "schedules", ["professional_id"], name: "index_schedules_on_professional_id", using: :btree
 
-  create_table "workplaces", force: :cascade do |t|
+  create_table "service_details", force: :cascade do |t|
+    t.integer  "duration"
+    t.integer  "price"
+    t.integer  "service_id"
+    t.integer  "professional_id"
+    t.integer  "shop_id"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_index "service_details", ["professional_id"], name: "index_service_details_on_professional_id", using: :btree
+  add_index "service_details", ["service_id"], name: "index_service_details_on_service_id", using: :btree
+  add_index "service_details", ["shop_id"], name: "index_service_details_on_shop_id", using: :btree
+
+  create_table "services", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "shop_profiles", force: :cascade do |t|
     t.string   "name"
     t.string   "address_1"
     t.string   "address_2"
@@ -114,8 +146,38 @@ ActiveRecord::Schema.define(version: 20150711225722) do
     t.string   "zip"
     t.string   "state"
     t.string   "country"
+    t.string   "image"
+    t.integer  "shop_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_index "shop_profiles", ["shop_id"], name: "index_shop_profiles_on_shop_id", using: :btree
+
+  create_table "shops", force: :cascade do |t|
+    t.string   "provider",                            null: false
+    t.string   "uid",                    default: "", null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.text     "tokens"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "shops", ["email"], name: "index_shops_on_email", using: :btree
+  add_index "shops", ["reset_password_token"], name: "index_shops_on_reset_password_token", unique: true, using: :btree
+  add_index "shops", ["uid", "provider"], name: "index_shops_on_uid_and_provider", unique: true, using: :btree
 
 end
