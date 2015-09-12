7c26fcc2f90f2b1770e9111d082911ba4f6d3f0c
Rohit Ahuja <rahuja95@gmail.com>
2015-09-12 04:47:32 -0400

    Fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,50 @@
 Rails.application.routes.draw do
+  ###
+  # Documentation Route
+  ###
   apipie
+
+  ###
+  # Devise Routes
+  ###
   devise_for :professional, only: []
-
+  # devise_for :shop, only: []
   mount_devise_token_auth_for 'Professional', at: 'api/v1/auth_professional'
+  # mount_devise_token_auth_for 'Shop', at: 'api/v1/auth_shop'
 
+  ###
+  # API Routes
+  ###
   namespace :api, defaults: { format: :json } do
+
+    ###
+    # V1 Routes
+    ###
     namespace :v1, shallow: true do
-      resources :professionals, only: [:show, :update] do
-      	resources :schedules, only: [:show, :update]
-      	resources :appointments, only: [:index, :show, :create, :update]
-      	resources :posts, only: [:index, :show, :create] # add destroy later
-      end
-      # Add routes for things accessible to all users 
-      # get :professionals, on: collection
-      # get :posts, on: :collection
+
+      ###
+      # Professional Routes
+      ###
+      # resource :professional, only: [] do
+      #   get :schedule, on: :member
+      #   get :appointments, on: :collection
+      # end
+      resources :professional_profiles, only: [:show, :update], path: '/professional/profiles'
+      
+      ###
+      # Shop Routes
+      ###
+      # resource :shop, only: [] do
+      #   get :professionals, on: :collection
+      # end
+      # resources :shop_profiles, only: [:show, :update], path: '/shop/profiles'
+
+      ###
+      # Additional Resource Routes
+      ###
+      # resources :appointments, only: [:show, :create, :update]
+      # resources :schedules, only: [:update]
+      resources :posts, only: [:index, :show, :create] # add destroy later
     end
   end
   
